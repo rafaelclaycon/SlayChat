be8abbe369fb4151ff54045ebd82a15895e686f2
Rafael Schmitt <rafael.claycon@gmail.com>
2023-11-15 14:23:14 -0300

    Initial Commit

---
+++ SlayChat/SlayChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlayChatApp.swift
+//  SlayChat
+//
+//  Created by Rafael Schmitt on 15/11/23.
+//
+
+import SwiftUI
+
+@main
+struct SlayChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
